469597e30cf33e6c00f84988b30bd32432f99fe0
Mahmoud Mustafa <mid7od@gmail.com>
2019-06-08 04:35:29 +0200

    display Popular posts

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -10,40 +10,46 @@ use App\Category;
 class BlogController extends Controller
 {
     //index function
-    public function index(){
-        $categories = Category::with('posts')->orderBy('title','asc')->get();
+    public function index()
+    {
+        $categories = Category::with('posts')->orderBy('title', 'asc')->get();
         // $posts = Post::with('author')->orderBy('created_at','desc')->get();
         // $posts = Post::with('author')->latest()->published()->paginate(3);
-        $posts = Post::with('author')->orderBy('published_at','desc')->published()->paginate(3);
-        return view('blog.index',compact('posts','categories'));
+        $posts = Post::with('author')->orderBy('published_at', 'desc')->published()->paginate(3);
+        return view('blog.index', compact('posts', 'categories'));
     }
     // category
-    public function category(Category $category){
+    public function category(Category $category)
+    {
         $categoryName = $category->title;
 
         $posts = $category->posts()
-                            ->with('author')
-                            ->latest()
-                            ->published()
-                            ->paginate(3);
+            ->with('author')
+            ->latest()
+            ->published()
+            ->paginate(3);
 
-        return view('blog.index',compact('posts','categoryName'));
+        return view('blog.index', compact('posts', 'categoryName'));
     }
     // author 
-    public function author(User $author){
+    public function author(User $author)
+    {
         $authorName = $author->name;
 
         $posts = $author->posts()
-                            ->with('category')
-                            ->latest()
-                            ->published()
-                            ->paginate(3);
+            ->with('category')
+            ->latest()
+            ->published()
+            ->paginate(3);
 
-        return view('blog.index',compact('posts','authorName'));
+        return view('blog.index', compact('posts', 'authorName'));
     }
     // show function
-    public function show(Post $post){
- 
-        return view('blog.show',compact('post'));
+    public function show(Post $post)
+    {
+        // increase view count
+        $post->increment('view_count');
+
+        return view('blog.show', compact('post'));
     }
 }
