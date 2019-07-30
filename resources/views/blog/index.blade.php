ea9a104241bc398149ae02b39fca881d05e68170
Mahmoud Mustafa <mid7od@gmail.com>
2019-07-30 15:39:34 +0200

    change post style & Adding post form

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -10,6 +10,33 @@
                 </div>
             </div>
             @endif
+            <div class="content mb-3">
+                <article class="post-item">
+                    <h5 class="p-4 font-weight-bold " style="color:#1d68a7;padding-bottom:.5rem !important;">
+                        Share Your Thought
+                    </h5>
+                    <form action="#" method="POST">
+                        @csrf
+                        <div class="form-group">
+                            <div class="col">
+                                <input class="form-control" type="text" name="title" id="title"
+                                    placeholder="Post Title...">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="col">
+                                <textarea class="form-control" name="text" id="text"
+                                    placeholder="Write Post Description Here..." rows="2"></textarea>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="col-md-4 offset-md-8 py-2">
+                                <button class="w-100 btn btn-primary" type="submit" >Post</button>
+                            </div>
+                        </div>
+                    </form>
+                </article>
+            </div>
             @if (!$posts->count())
             <div class="contnet">
                 <div class="alert alert-warning">
@@ -17,19 +44,22 @@
                 </div>
             </div>
             @else
-
             @foreach ($posts as $post)
-            <div class="content mb-2">
+            <div class="content mb-3">
                 <article class="post-item">
-                    <div class="post-meta p-3">
+                    <div class="post-meta p-3" style="padding-bottom: 0 !important;">
                         <ul class="post-meta-group">
                             <li>
-                                <h5 class="float-left font-weight-bold text-primary">
-                                    <div class="author-img">
-                                        <img src="/" alt="authorImg">
-                                    </div>
-                                    <a href="/author/{{ $post->author->slug}}">{{$post->author->name}}</a>
-                                </h5>
+                                <div class="author">
+                                    <a href="/author/{{ $post->author->slug}}">
+                                        <div class="author-img">
+                                            <img src="/img/user.svg" alt="authorImg">
+                                        </div>
+                                        <h5 class="float-left font-weight-bold " style="color:#1d68a7;">
+                                            {{$post->author->name}}
+                                        </h5>
+                                    </a>
+                                </div>
                             </li>
                             <span class="float-right">
                                 <li class="tag">
@@ -37,11 +67,11 @@
                                         {{$post->category->title}}</a></li>
                             </span>
                         </ul>
-
                     </div>
                     <div class="post-item-body ml-4">
                         <div class="p-4">
-                            <h2><a href="/blog/{{$post->id}}">{{$post->title}}</a></h2>
+                            <h4 style="font-weight:700;
+                            text-indent:30px;"><a href="/blog/{{$post->id}}">{{$post->title}}</a></h4>
                             {!!$post->excerpt_html!!}
                         </div>
                     </div>
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -6,6 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="csrf-token" content="{{ csrf_token() }}">
   <title>{{ __('Blog') }}</title>
+  <link href="https://fonts.googleapis.com/css?family=Dosis&display=swap" rel="stylesheet">
   <!-- Fonts -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.9.0/css/all.min.css">
   <link rel="dns-prefetch" href="//fonts.gstatic.com">
