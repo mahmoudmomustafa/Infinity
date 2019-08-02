f6b66265249e0e07d729ec376a2b6dcc52ce5b37
Mahmoud Mustafa <mid7od@gmail.com>
2019-08-02 20:21:58 +0200

    restyle users index

--- resources/views/backend/users/index.blade.php
+++ resources/views/backend/users/index.blade.php
@@ -1,32 +1,20 @@
 @extends('layouts.backend.main')
 @section('title','MyBlog | Users')
 @section('content')
-<div class="content-wrapper">
+<div class="container-fluid p-4">
   <!-- Content Header (Page header) -->
-  <section class="content-header">
-    <h1>
-      Users
-      <small>Display all Users</small>
-      <b>
-        <a href="/backend/users/create" class="btn btn-primary">
-          Add new User
-        </a>
-      </b>
+  <section class="content-header" style="overflow:auto">
+    <h1 class="float-left font-weight-bold " style="color:#1d68a7;">
+      Display All Users...
     </h1>
-    <ol class="breadcrumb">
-      {{-- Dashboard --}}
-      <li class="active">
-        <a href="{{route('dashboard')}}">Dashboard</a>
-      </li>
-      {{-- posts --}}
-      <li class="active">
-        <a href="/backend/users">Users</a>
-      </li>
-    </ol>
+    <div class="create float-right py-2">
+      <button class="btn btn-primary">
+        New User
+      </button>
+    </div>
   </section>
-
   <!-- Main content -->
-  <section class="container  mt-2">
+  <section class="container  mt-4">
     <div class="row">
       <div class="col">
         <div class="box">
@@ -42,39 +30,41 @@
               <strong>No record</strong>
             </div>
             @else
-            <table class="table taable-bordered">
-              <thead>
-                <tr>
-                  <td>Name</td>
-                  <td>Mail</td>
-                  <td width='80'>Post Count</td>
-                  <td>Role</td>
-                  <td width='80'>Actions</td>
-                </tr>
-              </thead>
-              <tbody>
-                @foreach ($users as $user)
-                <tr>
-                  <td>{{$user->name}}</td>
-                  <td>{{$user->email}}</td>
-                  <td>{{$user->posts->count()}}</td>
-                  <td>{{$user->role->name}}</td>
-                  <td style="display:flex;">
-                    <a href="/backend/users/{{$user->id}}/edit" class="mr-2 btn btn-xs btn-success">
-                      <i class="fa fa-edit"></i>
-                    </a>
-                    <form action="/backend/users/{{$user->id}}" method="post">
-                      @method('DELETE')
-                      @csrf
-                      <button type="submit" class="btn btn-xs btn-danger">
-                        <i class="fa fa-times"></i>
-                      </button>
-                    </form>
-                  </td>
-                </tr>
-                @endforeach
-              </tbody>
-            </table>
+            {{-- <div class="table-responsive"> --}}
+              <table class="table table-striped table-dark table-hover  table-bordered ">
+                <thead>
+                  <tr>
+                    <td scope="col">Name</td>
+                    <td scope="col">Mail</td>
+                    <td scope="col" width='80'>Post Count</td>
+                    <td scope="col" width='80'>Role</td>
+                    <td scope="col" width='80'>Actions</td>
+                  </tr>
+                </thead>
+                <tbody>
+                  @foreach ($users as $user)
+                  <tr>
+                    <td>{{$user->name}}</td>
+                    <td>{{$user->email}}</td>
+                    <td style="text-align:center;">{{$user->posts->count()}}</td>
+                    <td>{{$user->role->name}}</td>
+                    <td style="display:flex;">
+                      <a href="/backend/users/{{$user->id}}/edit" class="mr-2 btn btn-xs btn-success">
+                        <i class="fa fa-edit"></i>
+                      </a>
+                      <form action="/backend/users/{{$user->id}}" method="post">
+                        @method('DELETE')
+                        @csrf
+                        <button type="submit" class="btn btn-xs btn-danger">
+                          <i class="fa fa-times"></i>
+                        </button>
+                      </form>
+                    </td>
+                  </tr>
+                  @endforeach
+                </tbody>
+              </table>
+            {{-- </div> --}}
             @endif
           </div>
           <!-- /.box-body -->
